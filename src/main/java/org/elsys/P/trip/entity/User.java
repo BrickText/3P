06b1b8f86062c56bd964e2fd0847b854a925385f
nikolaystanishev <stanishev.nikolay@gmail.com>
2018-03-28 23:45:19 +0300

    Remove password from JSON serialization

--- src/main/java/org/elsys/P/trip/entity/User.java
+++ src/main/java/org/elsys/P/trip/entity/User.java
@@ -1,5 +1,7 @@
 package org.elsys.P.trip.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 import java.util.List;
@@ -15,6 +17,7 @@ public class User {
     private String username;
 
     @NotNull
+    @JsonIgnore
     private String password;
 
     @NotNull
