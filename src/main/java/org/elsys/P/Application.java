12156bc47c65645fcb60f7d4dece495c2bf21597
ivanmilevtues <ivanmilevtues@gmail.com>
2018-03-28 22:02:02 +0300

    Login features

--- src/main/java/org/elsys/P/Application.java
+++ src/main/java/org/elsys/P/Application.java
@@ -3,6 +3,7 @@ package org.elsys.P;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@EnableAutoConfiguration
 @SpringBootApplication
 public class Application {
 
---
+++ src/main/java/org/elsys/P/trip/SecurityConfiguration.java
@@ -0,0 +1,19 @@
+package org.elsys.P.trip;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+
+@Configuration
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.httpBasic()
+                .and().authorizeRequests()
+                .antMatchers("/index.html", "/").permitAll()
+                .anyRequest().authenticated().and().csrf().csrfTokenRepository(
+                CookieCsrfTokenRepository.withHttpOnlyFalse()
+        ).and().logout().logoutSuccessUrl("/");
+    }
+}
